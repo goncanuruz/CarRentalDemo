c40c014a2b48c36922fa47b48e734e0825df6362
goncanuruz <77647608+goncanuruz@users.noreply.github.com>
2021-03-03 03:53:15 +0300

    Add files via upload

---
+++ CarRental.sql
@@ -0,0 +1,49 @@
+﻿CREATE TABLE [dbo].[Brands] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+CREATE TABLE [dbo].[Users] (
+    [Id]        INT          IDENTITY (1, 1) NOT NULL,
+    [FirstName] VARCHAR (50) NOT NULL,
+    [LastName]  VARCHAR (50) NOT NULL,
+    [Email]     VARCHAR (50) NOT NULL,
+    [Password]  VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+CREATE TABLE [dbo].[Rentals] (
+    [Id]         INT      IDENTITY (1, 1) NOT NULL,
+    [CarId]      INT      NOT NULL,
+    [CustomerId] INT      NOT NULL,
+    [RentDate]   DATETIME NOT NULL,
+    [ReturnDate] DATETIME NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Rentals_Customers] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([Id]),
+    CONSTRAINT [FK_Rentals_Cars] FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([Id])
+);
+CREATE TABLE [dbo].[Customers] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [UserId]      INT          NOT NULL,
+    [CompanyName] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Customers_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+CREATE TABLE [dbo].[Colors] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Name] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+CREATE TABLE [dbo].[Cars] (
+    [Id]          INT          IDENTITY (1, 1) NOT NULL,
+    [BrandId]     INT          NOT NULL,
+    [ColorId]     INT          NOT NULL,
+    [Name]        VARCHAR (50) NOT NULL,
+    [Description] VARCHAR (50) NOT NULL,
+    [ModelYear]   INT          NOT NULL,
+    [DailyPrice]  MONEY        NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Cars_Brands] FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([Id]),
+    CONSTRAINT [FK_Cars_Colors] FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Colors] ([Id])
+);
+
